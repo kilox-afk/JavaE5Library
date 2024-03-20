0a76966c1ecf5d756d71a9375b32a1f2262e1f3b
kilox-afk <127036340+kilox-afk@users.noreply.github.com>
2024-03-20 21:55:28 +0100

    Delete src directory

--- src/Separateur.java
+++
@@ -1,8 +0,0 @@
-public class Separateur {
-    public static void afficherSeparateur() {
-        System.out.println("-----------------------------------------------------------------------------");
-    }
-    public static void afficherSeparateur2() {
-        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-    }
-}
